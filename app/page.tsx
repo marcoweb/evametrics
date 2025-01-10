fe777a822cb9e4bbc5bef2d673ca2a85c51fd7bd
Marco Carvalho <marcocarvalho.web@gmail.com>
2025-01-10 08:16:39 -0300

    MVP

--- app/page.tsx
+++ app/page.tsx
@@ -14,16 +14,19 @@ export default function Home() {
 
   const [resultProcess, setResultProcess] = useState([])
 
+  const [average, setAverage] = useState([])
+
   const handleDataSetUpload = (event) => {
     const file = event.target.files[0];
     const reader = new FileReader();
     reader.onload = (e) => {
       const text = e.target.result;
-      const rows = text.split('\n').map((row) => row.split(':'));
+      const rows = text.split('\n').map((row) => row.split(';'));
       const result = [];
-      rows.forEach(element => {
+      console.log(rows.slice(1))
+      rows.slice(1).forEach(element => {
         if (element[1] != undefined)
-          result.push({ id: element[0], terms: element[1].split(',').map(item => item.trim()) })
+          result.push({ id: 'D' + (element[0].length < 2 ? '0' : '') + element[0], title: element[1], terms: element[2].replace(/"/g, "").split(',').map(item => item.trim()) })
       });
       console.log(event.target.id)
       if (event.target.id == 'dataSetFile1')
@@ -70,6 +73,46 @@ export default function Home() {
       result.push(doc);
     });
     setResultProcess(result);
+    const av = [
+      {id : 'GoldIndex', countTerms: 0, averageTerms: 0, precisionAverage: null, recallAverage: null, fMeasureAverage: null},
+      {id : ds1Name, countTerms: 0, averageTerms: 0, precisionAverage: 0, recallAverage: 0, fMeasureAverage: 0},
+      {id : ds2Name, countTerms: 0, averageTerms: 0, precisionAverage: 0, recallAverage: 0, fMeasureAverage: 0}
+    ]
+    goldIndex.forEach((item) => {
+      av[0]['countTerms'] += item['terms'].length
+    });
+    av[0]['averageTerms'] = av[0]['countTerms'] / goldIndex.length
+    let ds1SumPrecision = 0
+    let ds1SumRecall = 0
+    let ds1SumFMeasure = 0
+    ds1Metrics.forEach(item => {
+      ds1SumPrecision += item['precision'].value
+      ds1SumRecall += item['recall'].value
+      ds1SumFMeasure += item['fMeasure'].value
+    });
+    ds1.forEach(item => {
+      av[1]['countTerms'] += item['terms'].length
+    })
+    av[1]['averageTerms'] = av[1]['countTerms'] / ds1.length
+    av[1]['precisionAverage'] = ds1SumPrecision / ds1Metrics.length
+    av[1]['recallAverage'] = ds1SumRecall / ds1Metrics.length
+    av[1]['fMeasureAverage'] = ds1SumFMeasure / ds1Metrics.length
+    let ds2SumPrecision = 0
+    let ds2SumRecall = 0
+    let ds2SumFMeasure = 0
+    ds2Metrics.forEach(item => {
+      ds2SumPrecision += item['precision'].value
+      ds2SumRecall += item['recall'].value
+      ds2SumFMeasure += item['fMeasure'].value
+    });
+    ds2.forEach(item => {
+      av[2]['countTerms'] += item['terms'].length
+    })
+    av[2]['averageTerms'] = av[2]['countTerms'] / ds2.length
+    av[2]['precisionAverage'] = ds2SumPrecision / ds2Metrics.length
+    av[2]['recallAverage'] = ds2SumRecall / ds2Metrics.length
+    av[2]['fMeasureAverage'] = ds2SumFMeasure / ds2Metrics.length
+    setAverage(av)
   }
 
   return (
@@ -149,9 +192,10 @@ export default function Home() {
       </form>
       {resultProcess.length > 0 ?
         <div>
+          <h2 className="p-4 font-bold text-2xl text-center">Resultados</h2>
           <table className='w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400'>
             <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
-              <tr>
+              <tr className='text-center'>
                 <th rowSpan={2}>Document Id</th>
                 <th colSpan={3}>{ds1Name}</th>
                 <th colSpan={3}>{ds2Name}</th>
@@ -171,6 +215,28 @@ export default function Home() {
               <td>{value['ds2'].precision.value}</td><td>{value['ds2'].recall.value}</td><td>{value['ds2'].fMeasure.value}</td></tr>))}
             </tbody>
           </table>
+
+          <h3 className="p-4 font-bold text-1xl text-center">Médias</h3>
+          
+          <table className='w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400'>
+            <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
+              <tr className='text-center'>
+                <th>Conjunto</th>
+                <th>Termos</th>
+                <th>Precission</th>
+                <th>Recall</th>
+                <th>FMeasure</th>
+              </tr>
+            </thead>
+            <tbody>
+              {average.map(value => (<tr key={value['id']} className='bg-white border-b dark:bg-gray-800 dark:border-gray-700'><td>{value['id']}</td>
+              <td className='text-center'>{value['averageTerms']}</td><td className='text-center'>{value['precisionAverage']}</td><td className='text-center'>{value['recallAverage']}</td><td className='text-center'>{value['fMeasureAverage']}</td></tr>))}
+              {/* {resultProcess.map(value => (<tr key={value['id']} className='bg-white border-b dark:bg-gray-800 dark:border-gray-700'><td>{value['id']}</td>
+              <td>{value['ds1'].precision.value}</td><td>{value['ds1'].recall.value}</td><td>{value['ds1'].fMeasure.value}</td>
+              <td>{value['ds2'].precision.value}</td><td>{value['ds2'].recall.value}</td><td>{value['ds2'].fMeasure.value}</td></tr>))} */}
+            </tbody>
+          </table>
+          <br />
         </div>
         : ''}
     </div>
--- app/services/metrics.services.ts
+++ app/services/metrics.services.ts
@@ -21,7 +21,9 @@ function getMetricsRecall(numberOfAssignedTerms : number, numberOfGoldIndexTerms
 }
 
 function getMetricsFMeasure(precision : number, recall : number) : IIndexingMetric {
-    const result = 2 * (precision * recall) / (precision + recall)
+    let result = 0
+    if (recall > 0)
+        result = 2 * (precision * recall) / (precision + recall)
     return {metric: MetricName.FMeasure, value: result, percentage: result * 100};
 }
 
@@ -29,7 +31,7 @@ export function getMetrics(datasetData) {
     const result = []
     datasetData.forEach((element) => {
         const precision = getMetricsPrecision(element.numberOfCorrectTerms, element.numberOfAssignedTerms);
-        const recall = getMetricsRecall(element.numberOfAssignedTerms, element.numberOfGoldIndexTerms);
+        const recall = getMetricsRecall(element.numberOfCorrectTerms, element.numberOfGoldIndexTerms);
         const fMeasure = getMetricsFMeasure(precision.value, recall.value);
         const doc = {
             id: element.id,
@@ -51,10 +53,10 @@ export function processDataSet(goldIndex, dataset) {
             numberOfAssignedTerms: element.terms.length,
             numberOfGoldIndexTerms: 0
         }
-        const gi =  goldIndex.find((obj) => obj.id == element.id);
+        const gi = goldIndex.find((obj) => obj.id == element.id);
         doc.numberOfGoldIndexTerms = gi.terms.length
         element.terms.forEach((item) => {
-            if(gi.terms.includes(item))
+            if(gi.terms.map((git) => {return git.toLowerCase()}).includes(item.toLowerCase()))
                 doc.numberOfCorrectTerms += 1;
         });
         result.push(doc)
